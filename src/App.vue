5b6952f9f224c7add8ecbca5225197151ccc46f3
Sebosun <sebos@protonmail.com>
2022-02-23 15:34:57 +0100

    Fonts imports, eczar, roboto, robotomono. global h1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1 class="title">Welcome to the best Cinema in Monterail</h1>
     <br />
     <Buttons />
   </div>
@@ -20,10 +20,21 @@ export default {
 <style lang="scss">
 @import "./normalize.css";
 @import "./variables.css";
+@import url("https://fonts.googleapis.com/css2?family=Eczar:wght@600&family=Roboto+Mono:wght@400;500&family=Roboto:wght@400;700&display=swap");
+
+:root {
+  font-family: "Roboto", sans-serif;
+}
+
+.title {
+  font-family: Eczar;
+  font-size: 5rem;
+  font-weight: 600;
+  line-height: 82px;
+  letter-spacing: -0.01em;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
