f6929146c3d4f51f32602f3d4515d6ce1b17ed53
Sebosun <sebos@protonmail.com>
2022-02-23 15:35:45 +0100

    applying roboto mono to buttons

--- src/components/UI/Buttons.vue
+++ src/components/UI/Buttons.vue
@@ -23,12 +23,16 @@
 
 .button {
   font-size: 1.125rem;
-  padding: 1em 2em;
+  padding: 1em 2.5em;
 
   color: inherit;
   border-radius: 999px;
   background: inherit;
   border: solid;
+
+  font-family: Roboto Mono;
+  font-weight: 500;
+  letter-spacing: 0.015em;
 
   &:hover {
     cursor: pointer;
@@ -38,11 +42,13 @@
     color: white;
     background: var(--color-brand);
     border-color: var(--color-brand);
+    line-height: 18px;
   }
 
   &__primary {
     color: var(--color-primary);
     border-color: var(--color-primary);
+    line-height: 18px;
   }
   &__primary__selected {
     color: white;
