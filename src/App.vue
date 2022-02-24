7416f4c948df7ef6929d95ee995d8f4ca4f78f92
Sebosun <sebos@protonmail.com>
2022-02-24 09:00:54 +0100

    header for medium sizes

--- src/App.vue
+++ src/App.vue
@@ -48,8 +48,6 @@ export default {
 }
 
 #app {
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,18 +2,33 @@ TODO: Get vue svg loader to load svgs
 <template>
   <header class="header">
     <MonteCinemaLogo />
-    <HamburgerMenu />
+    <button class="hamburgerButton">
+      <HamburgerMenu />
+    </button>
+    <nav class="header__navigation">
+      <ul>
+        <li>Contact Us</li>
+        <li>Screenings</li>
+        <li>Movies</li>
+      </ul>
+    </nav>
+    <div class="header__actions">
+      <Button variation="brand empty borderless">Register</Button>
+      <Button variation="brand">Login</Button>
+    </div>
   </header>
 </template>
 
 <script>
 import MonteCinemaLogo from "../assets/monte cinema.svg";
 import HamburgerMenu from "../assets/navigation-menu 1.svg";
+import Button from "./UI/Button.vue";
 
 export default {
   components: {
     MonteCinemaLogo,
     HamburgerMenu,
+    Button,
   },
 };
 </script>
@@ -21,6 +36,48 @@ export default {
 <style lang="scss">
 .header {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.375rem 1.5rem;
+  font-size: 1.125rem;
+
+  .hamburgerButton {
+    border: none;
+    background-color: inherit;
+  }
+
+  .hamburgerButton:hover {
+    cursor: pointer;
+  }
+
+  &__navigation {
+    display: none;
+  }
+  &__actions {
+    display: none;
+  }
+
+  @media (min-width: 840px) {
+    & .hamburgerButton {
+      display: none;
+    }
+
+    &__navigation {
+      display: block;
+      flex-grow: 1;
+      ul {
+        list-style-type: none;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        gap: 0.125rem;
+      }
+    }
+
+    &__actions {
+      display: flex;
+      flex-flow: wrap;
+    }
+  }
 }
 </style>
