1d31c798488631b075e336f125f9de93cb4fed50
Sebosun <sebos@protonmail.com>
2022-02-27 00:14:57 +0100

    tags component for genres

---
+++ src/components/UI/Tags.vue
@@ -0,0 +1,19 @@
+<script></script>
+
+<template>
+  <div class="tag background--rose">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.tag {
+  font-size: 14px;
+  font-weight: bold;
+  padding: 8px 16px;
+  border-radius: 24px;
+  max-width: fit-content;
+  color: var(--color-red);
+  background: var(--color-rose);
+}
+</style>
