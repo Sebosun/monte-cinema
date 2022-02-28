2a0b4e30453ca6bc9b155ed6808133c100375ef2
Sebosun <sebos@protonmail.com>
2022-02-28 12:40:10 +0100

    normal hover effects

--- src/components/HeaderNavigation.vue
+++ src/components/HeaderNavigation.vue
@@ -1,14 +1,59 @@
 <template>
   <nav>
     <ul>
-      <li>Contact Us</li>
-      <li>Screenings</li>
-      <li>Movies</li>
+      <li>
+        <router-link class="router-link-active" to="/contact">
+          Contact Us
+        </router-link>
+      </li>
+      <li>
+        <router-link class="router-link-active" to="/screenings"
+          >Screenings</router-link
+        >
+      </li>
+      <li>
+        <router-link class="router-link-active" to="/contact-us">
+          Contact Us
+        </router-link>
+      </li>
     </ul>
   </nav>
 </template>
 
 <style lang="scss" scoped>
+.router-link-active {
+  text-decoration: none;
+  color: inherit;
+}
+
+a {
+  position: relative;
+  transition: color 300ms ease-in-out;
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    height: 5px;
+    width: 135px;
+    left: -20px;
+    right: 0;
+    bottom: -48px;
+    background: var(--color-brand);
+    opacity: 0;
+    transition: opacity 300ms ease-in-out;
+  }
+
+  &:hover {
+    cursor: pointer;
+    font-weight: 700;
+    color: var(--color-brand);
+
+    &::before {
+      opacity: 1;
+    }
+  }
+}
+
 ul {
   list-style-type: none;
   display: flex;
@@ -18,10 +63,5 @@ ul {
 }
 li {
   color: var(--color-secondary);
-}
-li:hover {
-  cursor: pointer;
-  font-weight: 700;
-  color: var(--color-primary);
 }
 </style>
