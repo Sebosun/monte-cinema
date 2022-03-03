d0710e41ea5e7b9b46d7f70e8b5dc939a22fd990
Sebastian <51924529+Sebosun@users.noreply.github.com>
2022-03-03 11:01:18 +0100

    Hamburger update (#11)
    * hamburger menu functionality

--- src/components/HeaderNavigation.vue
+++ src/components/HeaderNavigation.vue
@@ -33,48 +33,65 @@
   }
 }
 
+ul {
+  padding: 0;
+  list-style-type: none;
+}
+
+@include media-sm {
+  nav {
+    background-color: #f7f7f7;
+  }
+  ul {
+    display: flex;
+    gap: 20px;
+    padding: 20px;
+    margin: 0;
+    padding-left: 1.5rem;
+  }
+}
+
 a {
   text-decoration: none;
   color: inherit;
 }
 
-ul {
-  padding: 0;
-  list-style-type: none;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  justify-content: stretch;
-  gap: 0.125rem;
-}
-
-.nav__hover {
-  position: relative;
-  transition: color 300ms ease-in-out;
-  color: var(--color-secondary);
-  justify-self: stretch;
-  text-align: center;
-
-  &::before {
-    content: "";
-    display: block;
-    position: absolute;
-    height: 5px;
-    width: 100%;
-    left: 0;
-    right: 0;
-    bottom: -50px;
-    background: var(--color-brand);
-    opacity: 0;
-    transition: opacity 300ms ease-in-out;
+@include media-md {
+  ul {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    justify-content: stretch;
+    gap: 0.125rem;
   }
-
-  &:hover {
-    cursor: pointer;
-    font-weight: 700;
-    color: var(--color-primary);
+  .nav__hover {
+    position: relative;
+    transition: color 300ms ease-in-out;
+    color: var(--color-secondary);
+    justify-self: stretch;
+    text-align: center;
 
     &::before {
-      opacity: 1;
+      content: "";
+      display: block;
+      position: absolute;
+      height: 5px;
+      width: 100%;
+      left: 0;
+      right: 0;
+      bottom: -50px;
+      background: var(--color-brand);
+      opacity: 0;
+      transition: opacity 300ms ease-in-out;
+    }
+
+    &:hover {
+      cursor: pointer;
+      font-weight: 700;
+      color: var(--color-primary);
+
+      &::before {
+        opacity: 1;
+      }
     }
   }
 }
