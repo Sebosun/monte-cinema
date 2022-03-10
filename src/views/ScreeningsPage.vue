a593e53762ac88bd0672919ad5bac9d1b815b63a
Sebosun <sebos@protonmail.com>
2022-03-10 12:18:22 +0100

    screenings page

--- src/views/ScreeningsPage.vue
+++ src/views/ScreeningsPage.vue
@@ -1,9 +1,18 @@
 <script>
-export default {};
+import MainHeader from "@/components/MainHeader.vue";
+import BreadcrumbNavigation from "@/components/chunks/BreadcrumbNavigation.vue";
+import Screenings from "@/components/sections/Screenings.vue";
+export default {
+  components: { MainHeader, BreadcrumbNavigation, Screenings },
+};
 </script>
 
 <template>
-  <div>Screenings Page</div>
+  <div class="screenings-page">
+    <main-header />
+    <breadcrumb-navigation>Screenings</breadcrumb-navigation>
+    <screenings />
+  </div>
 </template>
 
 <style scoped></style>
