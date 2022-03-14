0a71c80e9eb72cd43025a457a5aac8f0b4c35868
Sebosun <sebos@protonmail.com>
2022-03-14 11:22:59 +0100

    api get seances by id with time formatting

--- src/helpers/api/movies.js
+++ src/helpers/api/movies.js
@@ -8,4 +8,16 @@ export const getOneMovie = async (id) => defaultClient.get(`/movies/${id}`);
 export const getSeancesByMovieId = async (id) =>
   defaultClient.get(`/seances?movie_id=${id}`);
 
+// expects a date object
+export const getSeancesByDate = async (date) => {
+  // var year = date.getUTCFullYear();
+  // var month = `0${date.getUTCMonth() + 1}`.slice(-2);
+  // var day = `0${date.getDate()}`.slice(-2);
+  // var apiDateFormat = `${year}-${month}-${day}`; // yyyy-mm-dd
+  const dateIntl = new Intl.DateTimeFormat("pl-PL").format(date);
+  const apiDateFormat = dateIntl.replace("/", "-");
+  const response = await defaultClient.get(`/seances?date=${apiDateFormat}`);
+  return response;
+};
+
 export const getUser = async () => defaultClient.get("/user");
