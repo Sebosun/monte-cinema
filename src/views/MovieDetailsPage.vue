1e6f73d4347c68ea4ed8795983c8d9cb2bcab54c
Sebosun <sebos@protonmail.com>
2022-03-14 16:04:42 +0100

    clear candiate for a separate component

--- src/views/MovieDetailsPage.vue
+++ src/views/MovieDetailsPage.vue
@@ -2,19 +2,18 @@
 import ErrorMessage from "@/components/UI/ErrorMessage.vue";
 import MainHeader from "@/components/MainHeader.vue";
 import BreadcrumbNavigation from "@/components/chunks/BreadcrumbNavigation.vue";
-import Tags from "@/components/UI/Tags.vue";
+import MovieDescription from "@/components/chunks/MovieDescription.vue";
 import ScreeningsForOneMovie from "@/components/sections/ScreeningsForOneMovie.vue";
 
 import * as moviesApi from "@/helpers/api/movies";
-import movieLengthMinutesToHuman from "@/helpers/movieLengthMinutesToHuman.js";
 
 export default {
   components: {
     ErrorMessage,
     MainHeader,
     BreadcrumbNavigation,
-    Tags,
     ScreeningsForOneMovie,
+    MovieDescription,
   },
   data() {
     return {
@@ -31,25 +30,20 @@ export default {
     },
   },
   async created() {
-    try {
-      const response = await moviesApi.getOneMovie(this.id);
-      this.movie = response.data;
-    } catch (err) {
-      this.error = {
-        status: true,
-        message: "Unable to fetch movie. Please try again later.",
-      };
-    }
-    this.loading = false;
+    this.fetchMovie();
   },
-  computed: {
-    movieLength() {
-      return movieLengthMinutesToHuman(this.movie.length);
-    },
-    movieArray() {
-      const arr = [];
-      arr.push(this.movie);
-      return arr;
+  methods: {
+    async fetchMovie() {
+      try {
+        const response = await moviesApi.getOneMovie(this.id);
+        this.movie = response.data;
+      } catch (err) {
+        this.error = {
+          status: true,
+          message: "Unable to fetch movie. Please try again later.",
+        };
+      }
+      this.loading = false;
     },
   },
 };
@@ -66,126 +60,9 @@ export default {
       <BreadcrumbNavigation :nestedName="movie.title"
         >Movies</BreadcrumbNavigation
       >
-      <div class="movie-details__desc">
-        <div class="movie-details__meta">
-          <h1 class="font--header">{{ movie.title }}</h1>
-          <div class="movie-details__meta--misc">
-            <Tags>{{ movie.genre.name }}</Tags>
-            <div class="font--bold">1987</div>
-            <div class="font--bold">{{ movieLength }}</div>
-            <div class="font--bold">R</div>
-          </div>
-          <p>
-            {{ movie.description }}
-          </p>
-        </div>
-        <div class="movie-details__image">
-          <img :src="movie.poster_url" :alt="movie.title" />
-        </div>
-      </div>
+
+      <MovieDescription :movie="movie" />
       <ScreeningsForOneMovie :movie="movie" />
     </div>
   </div>
 </template>
-
-<style scoped lang="scss">
-.font--bold {
-  color: var(--color-secondary);
-}
-
-h1 {
-  color: var(--color-primary);
-}
-
-p {
-  font-family: Roboto Mono;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 22px;
-  line-height: 170%;
-}
-
-.movie-details {
-  &__meta {
-    margin-top: 20px;
-    display: flex;
-    flex-flow: column;
-    gap: 32px;
-    &--misc {
-      display: flex;
-      gap: 5px;
-      align-items: center;
-      /* done just so the dots are evenly spaceed */
-      div {
-        &:nth-child(n + 3) {
-          &:before {
-            padding-inline: 4px;
-            content: "  \2022";
-          }
-        }
-        &:nth-child(n + 2) {
-          margin-right: -4px;
-        }
-      }
-    }
-  }
-
-  &__image {
-    overflow: hidden;
-    height: 100%;
-    position: relative;
-  }
-  &__image img {
-    position: absolute;
-    width: 100%;
-    object-fit: cover;
-    object-position: center;
-  }
-
-  @include media-sm {
-    &__desc,
-    &__meta {
-      margin-inline: 24px;
-    }
-
-    h1 {
-      font-size: 48px;
-      margin: 0;
-    }
-
-    &__desc {
-      margin: 64px 0;
-      display: grid;
-      grid-template-rows: 1fr minmax(340px, 1fr);
-      .movie-details__image {
-        grid-row: 1;
-      }
-      gap: 28px;
-    }
-  }
-
-  @include media-md {
-    p {
-      font-size: 24px;
-      margin-bottom: auto;
-    }
-    h1 {
-      font-size: 80px;
-      margin: 0;
-    }
-
-    &__desc {
-      margin: 64px 0;
-      display: grid;
-      grid-template-columns: 1fr minmax(340px, 1fr);
-      gap: 28px;
-    }
-  }
-  @include media-lg {
-    &__desc {
-      grid-template-columns: 1fr minmax(600px, 1fr);
-      gap: 70px;
-    }
-  }
-}
-</style>
